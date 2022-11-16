d5a10d7fcd88204f2b55112ae86c606a9d780095
TacNayn <tacnayn.public@gmail.com>
2022-11-15 20:44:35 -0500

    Clarified what "value" was in the comments

--- Rainbow Lines/assets/minecraft/shaders/include/config_rainbow_lines.glsl
+++ Rainbow Lines/assets/minecraft/shaders/include/config_rainbow_lines.glsl
@@ -2,7 +2,7 @@
 #define COLOR_SHIFT_SPEED 400 // Higher is faster
 #define COLOR_WAVE_LENGTH 0.5 // Higher is shorter, can be above 1
 #define SATURATION 0.75 // Between 0 and 1
-#define VALUE 1.0 // Between 0 and 1
+#define VALUE 1.0 // HSV 'Value', changes the color brightness. Between 0 and 1
 
 // 0 - colors move away from player
 // 1 - colors move toward player
